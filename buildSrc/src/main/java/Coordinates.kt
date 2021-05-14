c9d0318b7831190f6e5f354402fe00f13020ff93
meliksahcakirr <meliksahcakirr@gmail.com>
2021-05-14 15:59:47 +0300

    Update workflow for deployment

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -3,8 +3,8 @@ const val PUBLISHING_GROUP = "com.meliksahcakir"
 object AppCoordinates {
     const val APP_ID = "com.meliksahcakir.spotialarm"
 
-    const val APP_VERSION_NAME = "1.1.0"
-    const val APP_VERSION_CODE = 1001000
+    const val APP_VERSION_NAME = "1.2.0_rc1"
+    const val APP_VERSION_CODE = 1002000
 }
 
 object LibraryAndroidCoordinates {

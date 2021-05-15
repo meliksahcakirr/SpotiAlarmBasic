a84a94f430ba400a9ec6d1effb966b9a6d112a77
meliksahcakirr <meliksahcakirr@gmail.com>
2021-05-15 14:09:44 +0300

    Release v1.2.0 & Add google play deployment workflow

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -3,7 +3,7 @@ const val PUBLISHING_GROUP = "com.meliksahcakir"
 object AppCoordinates {
     const val APP_ID = "com.meliksahcakir.spotialarm"
 
-    const val APP_VERSION_NAME = "1.2.0_rc1"
+    const val APP_VERSION_NAME = "1.2.0"
     const val APP_VERSION_CODE = 1002000
 }
 
